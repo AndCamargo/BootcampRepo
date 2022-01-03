2c9e34ba6bce7f7858bd36173d77fce36e6812cc
Andressa Camargo <annghell@gmail.com>
2022-01-03 12:08:42 -0300

    att aula integração git/github

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Welcome";
+            var b = " to Nekoland";
+            Console.WriteLine(a+b);
+        }
+    }
+}
